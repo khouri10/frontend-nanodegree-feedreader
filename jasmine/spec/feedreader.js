88c5f52dfd581d5e9b00cad4d0d3271502073251
Marcel <khouri10@gmail.com>
2017-12-09 14:39:31 -0200

    adicionar teste nas Entradas Iniciais: Contém pelo menos uma entrada

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -90,7 +90,29 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* New test suite for Initial Entries */
+    describe('Entradas iniciais', function() {
+
+        /* Test that ensures when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         * Remember, loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done() function.
+         */   
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('Contém pelo menos uma entrada', function(done) {
+        
+            var entry = $('.entry').html();
+            expect(entry).toBeDefined();
+            done();
+            
+        });
+    });
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
